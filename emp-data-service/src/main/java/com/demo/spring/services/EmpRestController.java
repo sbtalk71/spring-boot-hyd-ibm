3f55df9a08fefc0cfa27c7a381e2887ab2748ad7
sbtalk71 <shantanu@localhost>
2019-06-11 15:07:24 +0530

    day2 4

--- emp-data-service/src/main/java/com/demo/spring/services/EmpRestController.java
+++ emp-data-service/src/main/java/com/demo/spring/services/EmpRestController.java
@@ -5,9 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.spring.EmpRepository;
@@ -19,10 +20,13 @@ public class EmpRestController {
 	@Autowired
 	private EmpRepository repo;
 	
-	@RequestMapping(path="/emp/find/{id}",
+	/*@RequestMapping(path="/emp/find/{id}",
 			method=RequestMethod.GET,
-			produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity findEmp(@PathVariable("id")int id) {
+			produces=MediaType.APPLICATION_JSON_VALUE)*/
+	
+	@GetMapping(path="/emp/find",
+	produces= {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity findEmp(@RequestParam("id")int id) {
 		
 		Optional<Emp> o=repo.findById(id);
 		if(o.isPresent()) {
@@ -31,5 +35,19 @@ public class EmpRestController {
 			return ResponseEntity.ok("{\"status\":\"No Employee Found\"}");
 		}
 	}
+	
+	@PostMapping(path="/emp/save",
+			consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> saveEmp(@RequestBody Emp e){
+		
+		if(repo.existsById(e.getEmpId())) {
+			return ResponseEntity.ok("Employee exists in the database");
+		}else {
+			repo.save(e);
+			return ResponseEntity.ok("Employee saved successfully..");
+		}
+	}
+	
 
 }
